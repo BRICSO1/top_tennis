fa340a508ae07af0b2244ce80539e7e19887cc0a
bricso1 <173154570+BRICSO1@users.noreply.github.com>
2024-06-19 10:00:59 +0200

    Create connexion_account.php
    that a redirection for login.php and register.php

---
+++ connexion_account.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <title>Vous avez été connecté</title>
+  </head>
+  <body>
+    <p>
+      c'est bon vous avez été connnecté !
+    </p>
+    <a href="./top.php">retour au menu principale</a>
+  </body>
+</html>
