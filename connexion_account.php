d6bd5897e64a6a0ff2acfe3441279173d94de4e3
bricso1 <debrunarthur3@gmail.com>
2024-06-19 14:45:27 +0200

    correction de connexion.php
    sur les conseils de gulhe

--- connexion_account.php
+++ connexion_account.php
@@ -1,3 +1,20 @@
+<?php
+$dsn = 'mysql:host=localhost';'dbname=user';
+$username = 'root';
+$password = '';
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = 'SELECT * FROM users WHERE username = :username';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['username' => $username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+  
 <!DOCTYPE>
 <html>
   <head>
@@ -6,8 +23,19 @@
   </head>
   <body>
     <p>
-      c'est bon vous avez été connnecté !
+      <?php
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        echo 'Connexion réussie !';
+    } else {
+        echo 'Nom d\'utilisateur ou mot de passe incorrect.';
+    }
+  ?>
     </p>
     <a href="./top.php">retour au menu principal</a>
   </body>
 </html>
+  <?php
+}
+?>
