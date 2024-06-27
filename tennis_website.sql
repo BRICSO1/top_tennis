835f802add42f17880c3e383a4c58b02b2429a38
bricso1 <bricsoDev@gmail.com>
2024-06-27 11:09:01 +0200

    Add files via upload

---
+++ tennis_website.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 27 juin 2024 à 10:49
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tennis_website`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `players_of_tennis`
+--
+
+CREATE TABLE `players_of_tennis` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `points` varchar(50) NOT NULL,
+  `nationality` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `players_of_tennis`
+--
+
+INSERT INTO `players_of_tennis` (`id`, `firstname`, `lastname`, `points`, `nationality`) VALUES
+(1, 'Jannik', 'Sinner', '9525', 'Italien'),
+(2, 'Carlos', 'Alcaraz', '8560', 'Espagnol'),
+(3, 'Novak', 'Djokovic', '8360', 'Serbe'),
+(4, 'Alexender', 'Zverev', '6885', 'Allemand'),
+(5, 'Daniil', 'Medvedev', '6485', 'Russe'),
+(6, 'Andrey', 'Rublev', '4710', 'Russe'),
+(7, 'Casper', 'Ruud', '4025', 'Norvège'),
+(8, 'Hubert', 'Hurkacz', '3995', 'Pologne'),
+(9, 'Alex', 'De Minaur', '3845', 'Australie'),
+(10, 'Grigor', 'Dimitrov', '3775', 'Bulgarie');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `created_at`) VALUES
+(0, 'Bricso1', 'azerty', 'bricos1@exemple.uk', '0000-00-00 00:00:00'),
+(5, 'arthur', '$2y$10$qz5V2psul20k4MToD4HCCOJHC7sjj5ca0Z4eZJI1/eppsbheXi.ei', 'are@gjk.y', '2024-06-26 09:32:12');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `players_of_tennis`
+--
+ALTER TABLE `players_of_tennis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `players_of_tennis`
+--
+ALTER TABLE `players_of_tennis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
