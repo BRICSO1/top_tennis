6186545dbd2d88e1d2ce40809b3f5ec8cf468e96
bricso1 <debrunarthur3@gmail.com>
2024-07-27 17:44:55 +0200

    Update database.config.php

---
+++ config/database.config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'charset' => 'utf8mb4',
+    # 'host' => 'localhost',        // Adresse du serveur MySQL
+    # 'port' => 3306,               // Port du service
+    'host' => 'localhost:3306',     // Adresse du serveur MySQL avec port du service inclu
+    'dbname' => 'tennis_website',   // Nom de la base de données
+    'username' => 'root',           // Nom d'utilisateur MySQL
+    'password' => '',         // Mot de passe MySQL
+];
